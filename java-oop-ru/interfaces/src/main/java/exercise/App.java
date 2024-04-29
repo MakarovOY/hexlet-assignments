66172c57fd52d9bcd5db4aa413e37b65dd6c1ede
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-04-29 19:36:37 +0300

    Update App.java

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -4,5 +4,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
+public class App {
+    public static void main(String[] args) {
 
+
+    }
+    public  static List buildApartmentsList(List<Home> homes, int countOfFirstElements){
+
+
+
+       List<Home> homesToSort= new ArrayList<>(homes);
+       Collections.sort(homesToSort);
+       List<String> resultList = new ArrayList<>();
+        homesToSort.subList(0,countOfFirstElements).forEach(e-> resultList.add(e.toString()));
+
+        return resultList;
+
+    }
 // END
