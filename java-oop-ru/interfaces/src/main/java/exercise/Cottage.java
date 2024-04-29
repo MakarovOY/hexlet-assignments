a7fdc44587b60bc4aae248ababae75248d6f0565
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-04-29 19:37:21 +0300

    Update Cottage.java

--- java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
+++ java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
@@ -1,5 +1,34 @@
 package exercise;
 
 // BEGIN
+public class Cottage implements Home{
 
+    double area;
+    int floorCount;
+
+    public Cottage(double area, int floorCount) {
+        this.area = area;
+        this.floorCount = floorCount;
+    }
+
+    @Override
+    public double getArea() {
+        return area;
+    }
+
+    @Override
+    public int compareTo(Home another) {
+        double thisArea = area;
+        double anotherArea = another.getArea();
+
+        if(thisArea > anotherArea){
+            return 1;
+        } else if (thisArea < anotherArea) {
+            return -1;
+        } else return 0;
+    }
+    public String toString(){
+        return floorCount + " этажный коттедж площадью " + area + " метров";
+    }
+}
 // END
