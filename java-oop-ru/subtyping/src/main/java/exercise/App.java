ad2754e47c552218799f9dab3baeb1f6b194f8d3
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-05-01 21:11:17 +0300

    Update App.java

--- java-oop-ru/subtyping/src/main/java/exercise/App.java
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -1,19 +1,12 @@
 package exercise;
-import java.io.IOException;import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+
 
 // BEGIN
 public class App {
-   public static void swapKeyValue(KeyValueStorage keyValueStorageToSwap) {
-        keyValueStorageToSwap.toMap().forEach((k,v)->{
+    public static void swapKeyValue(KeyValueStorage keyValueStorageToSwap) {
+        keyValueStorageToSwap.toMap().forEach((k, v) -> {
             try {
                 keyValueStorageToSwap.unset(k);
             } catch (IOException e) {
