c3c37ed0e85d9bb74baf098e81669e2dcb8d46dc
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-05-01 21:23:46 +0300

    Update KeyValueStorage.java

--- java-oop-ru/subtyping/src/main/java/exercise/KeyValueStorage.java
+++ java-oop-ru/subtyping/src/main/java/exercise/KeyValueStorage.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 interface KeyValueStorage {
     void set(String key, String value);
-    void unset(String key);
+    void unset(String key) throws IOException;
     String get(String key, String defaultValue);
     Map<String, String> toMap();
 }
