5a5679f7884724c46280c2dff167ad2868fb2c33
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-05-12 16:46:19 +0300

    Update LabelTag.java

--- java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -1,5 +1,19 @@
 package exercise;
 
 // BEGIN
+public class LabelTag implements TagInterface {
 
+    String value;
+    TagInterface tag;
+
+    public LabelTag(String value, TagInterface tag) {
+        this.value = value;
+        this.tag = tag;
+    }
+
+    @Override
+    public String render() {
+
+        return String.format("<label>%s%s</label>", value, tag.render());
+    }
 // END
