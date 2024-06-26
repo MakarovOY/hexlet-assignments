506360217e539bc9b7eda624ca929e2ac418112d
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-06-26 18:02:29 +0300

    Update App.java

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -8,8 +8,8 @@ public final class App {
 
         // BEGIN
         var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
-        app.get("/phones", ctx -> ctx.json(Data.getPhones().toString()));
-        app.get("/domains", ctx -> ctx.json(Data.getDomains().toString()));
+        app.get("/phones", ctx -> ctx.json(Data.getPhones()));
+        app.get("/domains", ctx -> ctx.json(Data.getDomains()));
         return app;
         // END
     }
