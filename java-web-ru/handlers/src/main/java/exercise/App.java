f76f2f03d315a6a5a2a7e5fb0835af609faae877
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-06-26 17:58:19 +0300

    Update App.java

--- java-web-ru/handlers/src/main/java/exercise/App.java
+++ java-web-ru/handlers/src/main/java/exercise/App.java
@@ -7,7 +7,10 @@ public final class App {
     public static Javalin getApp() {
 
         // BEGIN
-        
+        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.start(7070);
         // END
     }
 
