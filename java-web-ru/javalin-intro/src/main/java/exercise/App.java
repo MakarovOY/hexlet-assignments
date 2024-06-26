d72e2980c2882cca861fbc33ddff32b6bcd9aac4
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-06-26 15:50:11 +0300

    Update App.java

--- java-web-ru/javalin-intro/src/main/java/exercise/App.java
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -10,7 +10,7 @@ public final class App {
 
         // BEGIN
         var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
-        app.get("/welcome/", ctx -> ctx.result("Welcome to Hexlet!)"));
+        app.get("/welcome/", ctx -> ctx.result("Welcome to Hexlet!"));
         return app;
         // END
     }
