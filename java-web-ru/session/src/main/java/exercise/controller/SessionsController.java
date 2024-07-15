ad9997f6487c13aacb8e0919465492dc64c4ce75
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-07-15 16:23:57 +0300

    Update SessionsController.java

--- java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -11,6 +11,36 @@ import io.javalin.http.Context;
 public class SessionsController {
 
     // BEGIN
-    
+private static final String CURRENT_USER = "currentUser";
+
+    public static void index(Context ctx) {
+
+        var page = new MainPage(ctx.sessionAttribute(CURRENT_USER));
+        ctx.render("index.jte", model("page", page));
+    }
+    public static void build(Context ctx) {
+
+        ctx.render("build.jte");
+    }
+
+    public static void create(Context ctx) {
+
+        var name = ctx.formParam("name");
+        var password = Security.encrypt(ctx.formParam("password"));
+        for (var user: UsersRepository.getEntities()) {
+            if(user.getPassword().equals(password) && user.getName().equals(name)) {
+                ctx.sessionAttribute(CURRENT_USER, name);
+                ctx.redirect(NamedRoutes.rootPath());
+                return;
+            }
+        }
+        var page = new LoginPage(name, "Wrong username or password");
+        ctx.render("build.jte", model("page", page));
+
+    }
+    public static void destroy(Context ctx) {
+        ctx.sessionAttribute(CURRENT_USER, null);
+        ctx.redirect(NamedRoutes.rootPath());
+    }    
     // END
 }
