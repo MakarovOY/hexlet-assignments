d6c3f29dd20e482848146e5ad5c21f129ae08d8c
MakarovOY <153019891+MakarovOY@users.noreply.github.com>
2024-07-15 16:24:27 +0300

    Update App.java

--- java-web-ru/session/src/main/java/exercise/App.java
+++ java-web-ru/session/src/main/java/exercise/App.java
@@ -16,7 +16,10 @@ public final class App {
         });
 
         // BEGIN
-        
+        app.get(NamedRoutes.rootPath(), SessionsController:: index);
+        app.get(NamedRoutes.buildSessionPath(), SessionsController:: build);
+        app.post(NamedRoutes.loginPath(), SessionsController:: create);
+        app.post(NamedRoutes.logoutPath(),SessionsController::destroy);        
         // END
 
         return app;
